539a6005e23793f53bc26090b4b006ed9b2e8b55
vishwastiwariii <vishwastiwariii09@gmail.com>
2025-11-29 12:20:53 +0530

    added routes

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ require('dotenv').config()
 const express = require ("express")
 const app = express()
 const mongoose = require('mongoose')
+const { userRouter } = require('./src/routes/user')
+const { adminRouter } = require('./src/routes/admin')
+const { vendorRouter } = require('./src/routes/vendor')
 
 
 mongoose.connect(process.env.MONGO_URL)
+
+app.use(express.json())
+
+app.use('/user',userRouter)
+app.use('/admin',adminRouter)
+app/use('/vendor',vendorRouter)
+
+app.listen(3000)
